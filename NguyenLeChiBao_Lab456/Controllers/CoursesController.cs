3b21ca05343249d8eec871a7af011c53a74f2a31
Administrator <Administrator@PC192>
2020-06-09 09:20:03 +0700

    dang ky dang nhap tao cot moi

--- NguyenLeChiBao_Lab456/Controllers/CoursesController.cs
+++ NguyenLeChiBao_Lab456/Controllers/CoursesController.cs
@@ -1,4 +1,5 @@
-﻿using NguyenLeChiBao_Lab456.Models;
+﻿using Microsoft.AspNet.Identity;
+using NguyenLeChiBao_Lab456.Models;
 using NguyenLeChiBao_Lab456.ViewModel;
 using System;
 using System.Collections.Generic;
@@ -16,14 +17,39 @@ namespace NguyenLeChiBao_Lab456.Controllers
             _dbContext = new ApplicationDbContext();
         }
         // GET: Courses
-        
+
+
+        [Authorize]
         public ActionResult Create()
         {
-            var ViewModel = new CouresViewModel
+            var ViewModel = new CoursesViewModel
             {
                 Categories = _dbContext.Categories.ToList()
             };
             return View(ViewModel);
+        }
+
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CoursesViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LecturerID = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryID = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home"); ;
         }
     }
 }
--- NguyenLeChiBao_Lab456/ViewModel/CoursesViewModel.cs
+++ NguyenLeChiBao_Lab456/ViewModel/CoursesViewModel.cs
@@ -1,16 +1,23 @@
 ﻿using NguyenLeChiBao_Lab456.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace NguyenLeChiBao_Lab456.ViewModel
 {
-    public class CouresViewModel
+    public class CoursesViewModel
     {
+        [Required]
         public string Place { get; set; }
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+        [Required]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
         public DateTime GetDateTime()
