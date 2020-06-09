8cb328d62db49653b3cd9338af6a358fed89046d
Administrator <Administrator@PC192>
2020-06-09 09:51:40 +0700

    chinh sua HomeController

--- NguyenLeChiBao_Lab456/Controllers/HomeController.cs
+++ NguyenLeChiBao_Lab456/Controllers/HomeController.cs
@@ -1,16 +1,31 @@
-﻿using System;
+﻿using NguyenLeChiBao_Lab456.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace NguyenLeChiBao_Lab456.Controllers
 {
     public class HomeController : Controller
     {
+
+        private ApplicationDbContext _dbContext;
+
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcommingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer )
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+            return View(upcommingCourses);
         }
 
         public ActionResult About()
